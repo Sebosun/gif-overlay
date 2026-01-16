66ecea243925c0be0cddc6979223e7d8345b49ff
bes <sebos@protonmail.com>
2026-01-16 15:19:11 +0100

    fixing scaling by factor of 2

--- lib/ratio.ts
+++ lib/ratio.ts
@@ -7,12 +7,14 @@ export const getRatio = (
   biggerElement: RatioDuck,
   smallerElement: RatioDuck,
 ): number => {
-  const DESIRED_MAX_RATIO = 0.12;
+  const DESIRED_MAX_RATIO = 0.25;
 
   const biggerSize = biggerElement.height * biggerElement.width;
   const smallerSize = smallerElement.height * smallerElement.width;
+
   const desiredSize = biggerSize * DESIRED_MAX_RATIO;
   const ratio = desiredSize / smallerSize;
+  const linearRatio = Math.sqrt(ratio);
 
   //   console.log(`
   // bigger: ${biggerSize}
@@ -21,5 +23,5 @@ export const getRatio = (
   // ratio: ${ratio}
   // result: ${ratio * smallerSize}`);
 
-  return ratio;
+  return linearRatio;
 };
